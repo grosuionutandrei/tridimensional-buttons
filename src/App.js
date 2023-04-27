878d43c2661f855532d1568f41a1e44b71e3c6e8
Andrei Ionut Grosu <ionutandreigrosu@yahoo.com>
2023-04-27 22:35:52 +0200

    style for hover behaviour

--- src/App.js
+++ src/App.js
@@ -13,27 +13,32 @@ import iconStyle from './presentation/icons.module.css'
 /* To do list => */
 /* {To Add state management for the hover event= in progress } */
 function App() {
+const order=['first','second' ,'third' ,'fourth'];
+
+
+
+
   return (
     <div className="App">
-     <Container className={ulStyle.container} > 
-      <Box>
+     <Container className={`${ulStyle.container}`} > 
+      <Box styleName={order[0]}>
         <Link href={'https://www.yahoo.com'} target={'blank'}>
          <FontAwesomeIcon icon={faEnvelope}  className={iconStyle.icon}/>
         </Link>
       </Box>
-      <Box>
+      <Box styleName={order[1]}>
        <Link>
          <FontAwesomeIcon icon={faUser} className={iconStyle.icon}>
          </FontAwesomeIcon>
        </Link>
       </Box>
-     <Box>
+     <Box styleName={order[2]}>
       <Link>
         <FontAwesomeIcon icon={faHouse} className={iconStyle.icon}>
         </FontAwesomeIcon>
       </Link>
      </Box>
-     <Box>
+     <Box styleName={order[3]}>
       <Link>
         <FontAwesomeIcon icon={faCloud} className={iconStyle.icon}>
         </FontAwesomeIcon>
--- src/icons/box.js
+++ src/icons/box.js
@@ -5,9 +5,10 @@ import React from 'react';
 import style from  '../presentation/box.module.css';
 
 export const Box=(props)=>{
+    console.log(props.styleName);
 
     return (
-        <li className={`${style.container} ${style.edges}`}>{props.children}
+        <li className={`${style.container} ${style.edges} ${style[props.styleName]}`}>{props.children}
    
         </li>
     )
